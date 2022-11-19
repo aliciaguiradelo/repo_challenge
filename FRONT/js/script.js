d10c9e87f214b6112baa96ccb96383275e546103
mariana-santos <marianasfernandessousa@gmail.com>
2022-11-19 17:41:42 -0300

    ajustando o cabeçalho e o rodapé de todas as páginas

---
+++ FRONT/js/script.js
@@ -0,0 +1,33 @@
+
+// DIMINUIR O MENU QUANDO O USUÁRIO SCROLLA
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    
+    if(scrollY>250){
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector("header").style.top = "0";
+            document.querySelector("header").style.paddingBottom = "3em";
+        } else {
+            document.querySelector("header").style.top = "-20vh";
+            document.querySelector("header").style.paddingBottom = "1em";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
+
+//MODAL
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+modal.classList.toggle("hide");
+fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+el.addEventListener("click", () => toggleModal());
+});
